bf7da1216b36ec8983a6f15e87774c8a63e7293a
gc535 <gc535@cornell.edu>
2019-01-12 06:39:53 -0700

    adding main test driver

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-#include <ServerBase.hpp>
+#include <HTTP_Server.hpp>
+#include <handler.hpp>
+
+int main()
+{   
+    WebFramework::Server<WebFramework::HTTP> http_server(12345, 1);
+    WebFramework::start_server<WebFramework::Server<WebFramework::HTTP> >(http_server);
+
+
+    return 0;
+}
